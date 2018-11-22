b2277f4133eb6cb3b0d8cffb8f738121af10a25e
jimi95 <1652877739@qq.com>
2018-11-22 14:10:12 +0800

    modify main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//来源： https://blog.csdn.net/mongo_node/article/details/80212122
+
 'use strict';
 var CryptoJS = require("crypto-js");
 var express = require("express");
@@ -8,6 +10,7 @@ var http_port = process.env.HTTP_PORT || 3001;
 var p2p_port = process.env.P2P_PORT || 6001;
 var initialPeers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 
+// 块结构 为了保证事情尽可能的简单，我们只选择最必要的部分：index（下标）、timestamp（时间戳）、data（数据）、hash（哈希值）和 previous hash（前置哈希值）。
 class Block {
     constructor(index, previousHash, timestamp, data, hash) {
         this.index = index;
@@ -25,20 +28,25 @@ var MessageType = {
     RESPONSE_BLOCKCHAIN: 2
 };
 
+//  存储区块 ：内存中的Javascript数组被用于存储区块链。区块链的第一个块通常被称为“起源块”，是硬编码的。
 var getGenesisBlock = () => {
     return new Block(0, "0", 1465154705, "my genesis block!!", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
 };
 
 var blockchain = [getGenesisBlock()];
 
+// 搭建一个HTTP服务器
 var initHttpServer = () => {
     var app = express();
     app.use(bodyParser.json());
 
     app.get('/blocks', (req, res) => res.send(JSON.stringify(blockchain)));
     app.post('/mineBlock', (req, res) => {
+        // 生成新的区块
         var newBlock = generateNextBlock(req.body.data);
+        // 添加新的区块
         addBlock(newBlock);
+        // 全网广播
         broadcast(responseLatestMsg());
         console.log('block added: ' + JSON.stringify(newBlock));
         res.send();
@@ -66,7 +74,7 @@ var initConnection = (ws) => {
     initMessageHandler(ws);
     initErrorHandler(ws);
     write(ws, queryChainLengthMsg());
-};
+};http_port
 
 var initMessageHandler = (ws) => {
     ws.on('message', (data) => {
@@ -95,8 +103,9 @@ var initErrorHandler = (ws) => {
     ws.on('error', () => closeConnection(ws));
 };
 
-
+// 新的区块的生成
 var generateNextBlock = (blockData) => {
+    //获取上一个块的hash
     var previousBlock = getLatestBlock();
     var nextIndex = previousBlock.index + 1;
     var nextTimestamp = new Date().getTime() / 1000;
@@ -109,16 +118,20 @@ var calculateHashForBlock = (block) => {
     return calculateHash(block.index, block.previousHash, block.timestamp, block.data);
 };
 
+
+// SHA-256会对块的内容进行加密，记录这个值应该和“挖矿”毫无关系，因为这里不需要解决工作量证明的问题。
 var calculateHash = (index, previousHash, timestamp, data) => {
     return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 };
 
+//将区块写入链中
 var addBlock = (newBlock) => {
     if (isValidNewBlock(newBlock, getLatestBlock())) {
         blockchain.push(newBlock);
     }
 };
 
+// 确认块的完整性  在我们从其他节点接收到新的区块，并需要决定接受或拒绝它
 var isValidNewBlock = (newBlock, previousBlock) => {
     if (previousBlock.index + 1 !== newBlock.index) {
         console.log('invalid index');
@@ -166,6 +179,7 @@ var handleBlockchainResponse = (message) => {
     }
 };
 
+//  选择最长的链
 var replaceChain = (newBlocks) => {
     if (isValidChain(newBlocks) && newBlocks.length > blockchain.length) {
         console.log('Received blockchain is valid. Replacing current blockchain with received blockchain');
@@ -191,6 +205,7 @@ var isValidChain = (blockchainToValidate) => {
     return true;
 };
 
+//通过下标获取上个区块 方便获取上个区块的hash值
 var getLatestBlock = () => blockchain[blockchain.length - 1];
 var queryChainLengthMsg = () => ({'type': MessageType.QUERY_LATEST});
 var queryAllMsg = () => ({'type': MessageType.QUERY_ALL});
@@ -203,6 +218,7 @@ var responseLatestMsg = () => ({
 });
 
 var write = (ws, message) => ws.send(JSON.stringify(message));
+// 全网广播
 var broadcast = (message) => sockets.forEach(socket => write(socket, message));
 
 connectToPeers(initialPeers);
